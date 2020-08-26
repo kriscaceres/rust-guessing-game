a67d9c68b7ce233b061dde5965e4520a41de6307
Kris <thesalmoneffect@gmail.com>
2020-08-26 12:20:38 -0400

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    /*
+    println!("Guess the number!");
+    let secret_num=rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is: {}", secret_num);
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    */
+    println!("You guessed: {}", guess);
+
+}
